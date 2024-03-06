d8b115c33ccf3ca37f5193fef041657ab8463f34
mo1333 <michi.oster23@gmail.com>
2024-03-06 10:22:40 +0100

    includes model logger

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 import ignite
 import torch as th
@@ -13,12 +14,14 @@ from torch.utils.data import DataLoader
 # following https://github.com/Project-MONAI/tutorials/blob/818673937c9c5d0b0964924b056a867238991a6a/3d_segmentation/unet_segmentation_3d_ignite.ipynb#L102
 # https://colab.research.google.com/drive/1wy8XUSnNWlhDNazFdvGBHLfdkGvOHBKe#scrollTo=uHAA3LUxD2b6
 
+now_str = datetime.now().strftime("%Y_%m_%d__%H_%M_%S")
+
 device_str = "cuda" if th.cuda.is_available() else "cpu"
 device = th.device(device_str)
 epochs = 1
 batch_size = 8
-new_image_size = (160, 160)  # make smaller to use on Laptop
-log_dir = "experiments/logs/"
+new_image_size = (16, 16)  # make smaller to use on Laptop
+exp_path = "experiments/" + now_str + "/"
 
 transformer = Compose([LoadImage(image_only=True),
                        EnsureChannelFirst(),
@@ -56,6 +59,17 @@ model = UNet(
 opt = th.optim.Adam(model.parameters(), 1e-3)
 loss = DiceLoss(sigmoid=True)
 trainer = ignite.engine.create_supervised_trainer(model, opt, loss, device, False)
-train_tensorboard_stats_handler = TensorBoardStatsHandler(log_dir=log_dir, output_transform=lambda x: x)
+
+# Record the loss
+train_tensorboard_stats_handler = TensorBoardStatsHandler(log_dir=exp_path, output_transform=lambda x: x)
 train_tensorboard_stats_handler.attach(trainer)
+
+# Save the current model
+checkpoint_handler = ignite.handlers.ModelCheckpoint(exp_path, "net", n_saved=1, require_empty=False)
+trainer.add_event_handler(
+    event_name=ignite.engine.Events.EPOCH_COMPLETED,
+    handler=checkpoint_handler,
+    to_save={"net": model, "opt": opt},
+)
+
 trainer.run(train_dataloader, epochs)
