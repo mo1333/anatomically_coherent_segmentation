435c71a22806a8a618b33db3e52c6eecbc65dadc
mo1333 <michi.oster23@gmail.com>
2024-03-20 13:47:16 +0100

    alternative training pipeline

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import json
 import os
 from datetime import datetime
+from tqdm import tqdm
 
 import ignite
 import torch as th
@@ -105,84 +106,94 @@ def train():
     ).to(device)
 
     opt = th.optim.Adam(model.parameters(), 1e-3)
-    loss = TotalLoss(loss_config)
+    loss_func = TotalLoss(loss_config)
 
     # ----------------
     # --- TRAINING ---
     # ----------------
 
-    trainer = ignite.engine.create_supervised_trainer(model,
-                                                      opt,
-                                                      loss,
-                                                      device,
-                                                      False,
-                                                      output_transform=lambda x, y, y_pred, loss: (x, y, y_pred, loss))
-    writer = SummaryWriter(log_dir=exp_path)
-
-    # Record train loss
-    train_tb_stats_handler = TensorBoardStatsHandler(log_dir=exp_path,
-                                                     summary_writer=writer,
-                                                     tag_name="train loss",
-                                                     output_transform=lambda output: output[3].item())  # output[3] = loss
-    train_tb_stats_handler.attach(trainer)
-
-    # Record validation dice metric
-    metric = MeanDice()
-    val_metric = {"score": metric}
-    evaluator = ignite.engine.create_supervised_evaluator(
-        model,
-        val_metric,
-        device,
-        True
-    )
-
-    val_tb_stats_handler = TensorBoardStatsHandler(log_dir=exp_path,
-                                                   summary_writer=writer,
-                                                   tag_name="validation loss")
-    val_tb_stats_handler.attach(evaluator)
-
-    @trainer.on(ignite.engine.Events.EPOCH_COMPLETED())
-    def run_intrain_val(engine):
-        evaluator.run(val_dataloader)
-
-    # Record example images from
-    train_tb_image_handler = TensorBoardImageHandler(log_dir=exp_path,
-                                                     summary_writer=writer,
-                                                     output_transform=lambda output: output[2][0])  # output[2] = y_pred
-
-    train_tb_image_handler.attach(trainer)
-
-    # Save the current model
-    checkpoint_handler = ignite.handlers.ModelCheckpoint(exp_path, "net", n_saved=1, require_empty=False)
-    trainer.add_event_handler(
-        event_name=ignite.engine.Events.EPOCH_COMPLETED,
-        handler=checkpoint_handler,
-        to_save={"trainer": trainer,
-                 "net": model,
-                 "opt": opt}
-    )
-
-    ProgressBar(persist=False).attach(trainer)
-    trainer.run(train_dataloader, epochs)
-
+    # trainer = ignite.engine.create_supervised_trainer(model,
+    #                                                   opt,
+    #                                                   loss,
+    #                                                   device,
+    #                                                   False,
+    #                                                   output_transform=lambda x, y, y_pred, loss: (x, y, y_pred, loss))
     # writer = SummaryWriter(log_dir=exp_path)
-    # for epoch in tqdm(range(epochs)):
-    #     model.train()
-    #     epoch_loss = 0
-    #     step = 0
-    #     for batch_data in train_dataloader:
-    #         step += 1
-    #         inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
-    #         opt.zero_grad()
-    #         outputs = model(inputs)
-    #         loss = loss_func(outputs, labels)
-    #         loss.backward()
-    #         opt.step()
-    #         epoch_loss += loss.item()
-    #         epoch_len = len(train_data) // train_dataloader.batch_size
-    #         writer.add_scalar("train loss", loss.item(), epoch_len * epoch + step)
     #
-    # writer.close()
+    # # Record train loss
+    # train_tb_stats_handler = TensorBoardStatsHandler(log_dir=exp_path,
+    #                                                  summary_writer=writer,
+    #                                                  tag_name="train loss",
+    #                                                  output_transform=lambda output: output[3].item())  # output[3] = loss
+    # train_tb_stats_handler.attach(trainer)
+    #
+    # # Record validation dice metric
+    # metric = MeanDice()
+    # val_metric = {"validation score": metric}
+    # evaluator = ignite.engine.create_supervised_evaluator(
+    #     model,
+    #     val_metric,
+    #     device,
+    #     True
+    # )
+    #
+    # val_tb_stats_handler = TensorBoardStatsHandler(log_dir=exp_path,
+    #                                                summary_writer=writer,
+    #                                                tag_name="validation loss")
+    # val_tb_stats_handler.attach(evaluator)
+    #
+    # @trainer.on(ignite.engine.Events.EPOCH_COMPLETED())
+    # def run_intrain_val(engine):
+    #     evaluator.run(val_dataloader)
+    #
+    # # Record example images from
+    # train_tb_image_handler = TensorBoardImageHandler(log_dir=exp_path,
+    #                                                  summary_writer=writer,
+    #                                                  output_transform=lambda output: output[2][0])  # output[2] = y_pred
+    #
+    # train_tb_image_handler.attach(trainer)
+    #
+    # # Save the current model
+    # checkpoint_handler = ignite.handlers.ModelCheckpoint(exp_path, "net", n_saved=1, require_empty=False)
+    # trainer.add_event_handler(
+    #     event_name=ignite.engine.Events.EPOCH_COMPLETED,
+    #     handler=checkpoint_handler,
+    #     to_save={"trainer": trainer,
+    #              "net": model,
+    #              "opt": opt}
+    # )
+    #
+    # ProgressBar(persist=False).attach(trainer)
+    # trainer.run(train_dataloader, epochs)
+
+    writer = SummaryWriter(log_dir=exp_path)
+    for epoch in tqdm(range(epochs)):
+        model.train()
+        step = 0
+        for batch_data in train_dataloader:
+            step += 1
+            inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
+            opt.zero_grad()
+            outputs = model(inputs)
+            loss = loss_func(outputs, labels)
+            loss.backward()
+            opt.step()
+            epoch_len = len(train_data) // train_dataloader.batch_size
+            writer.add_scalar("train loss", loss.item(), epoch_len * epoch + step)
+
+        model.eval()
+        val_loss = 0
+        for batch_data in val_dataloader:
+            inputs, labels = batch_data[0].to(device), batch_data[1].to(device)
+            outputs = model(inputs)
+            loss = loss_func(outputs, labels)
+            val_loss += loss.item()
+        writer.add_scalar("validation loss", val_loss, epoch)
+        writer.add_image("sample output channel 1", outputs[0, 1])
+        writer.add_image("sample output channel 2", outputs[0, 2])
+
+
+    writer.close()
 
     # ------------------
     # --- EVALUATION ---
