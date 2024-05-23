fixing rotation error in evaluation of polar experiments

evaluate_util.py
@@ -208,7 +208,7 @@ def evaluate_polar_model(config, best_threshold_per_channel, metric, model, writ
             plt.imshow(polar_labels[0].permute(1, 2, 0).numpy())
             plt.title(names[j])
             plt.savefig(save_name + "polar_labels.png")
-            plt.imshow(np.transpose(output, (2, 1, 0)))
+            plt.imshow(np.transpose(output, (1, 2, 0)))
             plt.title(names[j])
             plt.savefig(save_name + "output.png")
             plt.imshow(np.transpose(output_cartesian[0], (1, 2, 0)))