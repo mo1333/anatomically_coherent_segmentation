906800e5cfb1069d84dc084881df55cdb449b1ac
mo1333 <michi.oster23@gmail.com>
2024-08-24 07:58:15 +0200

    novel topological loss V3

--- loss.py
+++ loss.py
@@ -78,7 +78,7 @@ class NaiveTopologyLoss(_Loss):
         func_dict = {"": None,
                      "square": torch.square,
                      "exp": lambda x: torch.sub(torch.exp(x), 1),
-                     "log": lambda x: torch.log(torch.add(x, 1))}
+                     "log": lambda x: - torch.log(torch.sub(1, x))}
 
         if self.sigmoid:
             y = torch.sigmoid(input)
